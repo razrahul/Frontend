a43131dc7fa4dd362dcc6f644f4342108da01e1a
Rahul Kumar <158333668+razrahul@users.noreply.github.com>
2024-08-27 12:25:24 +0530

    # create at Add

--- src/components/Admin/Users/SubcriptionItem.jsx
+++ src/components/Admin/Users/SubcriptionItem.jsx
@@ -19,6 +19,7 @@ import {
   Td,
   TableCaption,
 } from "@chakra-ui/react";
+import { m } from "framer-motion";
 
 function OpenSubscriptionItem({ isOpen, onClose, user, loading, courses }) {
 
@@ -26,6 +27,30 @@ function OpenSubscriptionItem({ isOpen, onClose, user, loading, courses }) {
     const course = courses.find((course) => course._id === id);
     return course ? course.title : "Not Found";
   };
+
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const formattedDate = new Intl.DateTimeFormat('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true,
+    }).format(date);
+  //  console.log(formattedDate)
+    // Split the formatted date into parts
+    const [datePart, timePart] = formattedDate.split(' at ');
+    const [monthday, year] = datePart.split(',');
+    const [month, day] = monthday.split(' ');
+    
+    // Return the formatted string with "At" capitalized
+    return `${day} ${month} ${year}, At ${timePart}`;
+  };
+  
+  
+  
 
   return (
     <Modal
@@ -57,12 +82,15 @@ function OpenSubscriptionItem({ isOpen, onClose, user, loading, courses }) {
             <Text fontWeight="bold">User_ID: {user._id}</Text>
             <Text fontWeight="bold">Email: {user.email}</Text>
             <Text fontWeight="bold">Role: {user.role}</Text>
+            <Text fontWeight="bold">
+              Created At: {formatDate(user.createdAt)}
+            </Text>
           </Box>
 
           {user.role === "admin" ? (
             <Heading
               textTransform={"uppercase"}
-              children="User is an admin and has access to all subscriptions."
+              children="Admin has access to all subscriptions."
               my="20"
               textAlign={"center"}
             />
@@ -78,7 +106,7 @@ function OpenSubscriptionItem({ isOpen, onClose, user, loading, courses }) {
                     </TableCaption>
                     <Thead>
                       <Tr>
-                        <Th>Subscription ID</Th>
+                        <Th> ID</Th>
                         <Th>Course ID</Th>
                         <Th>Course Name</Th>
                         <Th>Status</Th>
@@ -88,7 +116,7 @@ function OpenSubscriptionItem({ isOpen, onClose, user, loading, courses }) {
                     <Tbody>
                       {user.subscription.map((sub) => (
                         <Tr key={sub._id}>
-                          <Td>{sub._id}</Td>
+                          <Td>#{sub._id}</Td>
                           <Td>{sub.course}</Td>
                           <Td>{CourseHandler(sub.course)}</Td>
                           <Td>{sub.status}</Td>
