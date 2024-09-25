a1b922ac850d2723fddfcdc60248356188cf6456
Rahul Kumar <razrahueee@gmail.com>
2024-09-25 16:20:30 +0530

    cors change

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // proxy:{
-    //   '/api/v1': {
-    //     target: 'https://backend-p5pz.onrender.com',
-    //     changeOrigin: true,
-    //     secure: false,
-    //   }
-    // },
-    watch: {
-      usePolling: true,
-    },
-  },
+  // server: {
+  //   // proxy:{
+  //   //   '/api/v1': {
+  //   //     target: 'https://backend-p5pz.onrender.com',
+  //   //     changeOrigin: true,
+  //   //     secure: false,
+  //   //   }
+  //   // },
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  // },
 })
